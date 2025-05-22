1802460d5b8db17bed2a092130dd9e0560e20aa3
TreltaSev <treltasev@gmail.com>
2025-05-21 20:25:12 -0400

    docs(update): Using the new doc system

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,28 +1,43 @@
 <script lang="ts">
-	import { compile } from 'mdsvex';
+	// --- Components ---
+	import { Flex, Header } from 'sk-clib';
+	import { DocsHandler } from '@doc/handler';
 
-	import { mdsvex_config } from '@config';
-	import { onMount, type Snippet } from 'svelte';
+	const handler = new DocsHandler();
+	const docs = handler.docs;
 
-    let html = $state(``)
-
-	let child: any | undefined = $state(undefined)
-	
-
-	onMount(async () => {
-
-		console.log(import.meta.glob('./lib/docs/**/*.{txt}', {eager: true}))
-		
-
-		// let x = await import('@doc/+page.svx')
-		// child = x
-
-
-		// setTimeout(() => {
-		// 	console.log(child)
-		// }, 100)
-	});
+	// Abide to the order of the doc object
+	function getSortedDocs(obj: Record<string, any>) {
+		return Object.entries(obj)
+			.filter(([key]) => key !== 'root')
+			.sort(([, a], [, b]) => {
+				console.log(a, b)
+				const order_a = a?.root?.metadata?.order ?? a?.metadata?.order ?? 9999;
+				const order_b = b?.root?.metadata?.order ?? b?.metadata?.order ?? 9999;
+				return order_a - order_b;
+			});
+	}
 </script>
 
-<span>Testing direct compilation</span>
-<!-- {@render child?.default?.()} -->
+{#if docs.root}
+	<Header xxl>{docs.root.metadata.title}</Header>
+{/if}
+
+{#each getSortedDocs(docs) as [key, value]}
+	<Flex col>
+		{#if value.root}
+			<Header md>{value.root.metadata.root} {key}</Header>
+			<svelte:component this={value.root.component}/>
+		{/if}
+
+		<!-- Rendering Subfolders/files -->
+		{#each getSortedDocs(value) as [sub_key, sub_value]}
+			{#if sub_value.root}
+				<svelte:component this={sub_value.root.component} />
+			{:else}
+				<svelte:component this={sub_value.component} />
+			{/if}
+		{/each}
+	</Flex>
+{/each}
+
